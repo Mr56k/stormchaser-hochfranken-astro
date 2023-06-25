88229c98ec06bc632d9bbb43f4ce0365b863f75a
Tobias Feigel <info@feigel.it>
2023-06-25 15:25:09 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()]
+  integrations: [image(), tailwind()],
+  outDir: './dist/httpdocs'
+
 });
