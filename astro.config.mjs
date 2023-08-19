d8bb219c293f093f0087a78dcdceeb6fc4780b44
Tobias Feigel <info@feigel.it>
2023-08-19 11:27:50 +0200

    Node Adapter hinzugefügt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
-
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 
@@ -9,4 +9,7 @@ import markdoc from "@astrojs/markdoc";
 export default defineConfig({
   integrations: [image(), tailwind(), react(), markdoc()],
   output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
