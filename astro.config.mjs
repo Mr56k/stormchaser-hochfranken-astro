e48349f480bf66fb148839bc87821caba539ba9a
Tobias Feigel <info@feigel.it>
2023-08-19 11:11:02 +0200

    Keystatic hinzugefügt (mit Testausgabe)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), tailwind()],
-  outDir: './dist/httpdocs'
-
+  integrations: [image(), tailwind(), react(), markdoc()],
+  output: 'hybrid',
 });
